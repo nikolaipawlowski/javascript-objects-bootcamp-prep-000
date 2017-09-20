470b19577f7a65db7bff563e0e4baba7e9bc9942
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-20 19:50:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName];
+=======
+  delete playlist.artistName
+>>>>>>> f81bbb4e9dc930f0678162e360106656acc923d1
   return playlist
 }
